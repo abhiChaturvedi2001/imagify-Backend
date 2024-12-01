a394145e10097512f09df2e1918a58c760ef1b8a
Abhishek Chaturvedi <65055220+abhiChaturvedi2001@users.noreply.github.com>
2024-12-01 19:57:24 +0530

    Update index.js
    adding url cors

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const authRouter = require("./routes/authRoutes")
 const app = express();
 dotenv.config({})
 const options = {
-    origin: "http://localhost:5173",
+    origin: "https://imagify-vert-eight.vercel.app",
     credentials: true
 }
 
